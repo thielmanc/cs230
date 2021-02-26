7cef44abd95882b7cfc321645309c268ee6f2338
thielmanc <cmt0024@mix.wvu.edu>
2021-02-26 00:49:39 -0500

    about page

---
+++ index.php
@@ -0,0 +1,54 @@
+<main>
+    <link rel="stylesheet" href="css/about.css">
+    
+    <div class="about">
+        <h1>About Us</h1>
+        <p>Sample text sample text sample text</p>
+    </div>
+    <div class="row">
+
+        <div class="column">
+            <div class="card">
+                <img src="images/1.jpeg">
+                <div class="container">
+                    <h2>CEO</h2>
+                    <p>Tom Brady</p>
+                    <p>Email: tb12@gmail.com</p>  
+                </div>
+            </div>
+        </div>
+
+        <div class="column">
+            <div class="card">
+                <img src="images/2.jpeg">
+                <div class="container">
+                    <h2>CEO</h2>
+                    <p>Bob Huggins</p>
+                    <p>Email: huggybear@gmail.com</p>
+                </div>
+            </div>
+
+        </div>  <div class="column">
+            <div class="card">
+                <img src="images/3.jpeg">
+                <div class="container">
+                    <h2>CEO</h2>
+                    <p>Bobby Shmurda</p>
+                    <p>Email: gs9bobby@gmail.com</p>
+                </div>
+            </div>
+
+        </div>  <div class="column">
+            <div class="card">
+                <img src="images/4.jpeg">
+                <div class="container">
+                    <h2>CEO</h2>
+                    <p>Larry the Lobster</p>
+                    <p>Email: livinlikelarry@gmail.com</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+</main>
